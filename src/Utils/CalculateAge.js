e93d84b0776e8ec96970913ee259a6e5b956a490
pareshrocky <pareshrockyphry20002016@gmail.com>
2024-11-09 13:15:33 +0530

    Age calculaion logic

---
+++ src/Utils/CalculateAge.js
@@ -0,0 +1,24 @@
+export const calculateAge = (userInputDate) => {
+  const { day, month, year } = userInputDate;
+  const inputDate = new Date(year, month - 1, day); // month is 0-indexed
+  const currentDate = new Date();
+  let years = currentDate.getFullYear() - inputDate.getFullYear();
+  let months = currentDate.getMonth() - inputDate.getMonth();
+  let days = currentDate.getDate() - inputDate.getDate();
+  if (days < 0) {
+    months--;
+    days += new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    ).getDate();
+  }
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+  if (!isNaN(years) && years >= 0) {
+    return {days,months,years}
+  } 
+  return null;
+};
